42c1c1a8c009ac1b96480a87d01685151c249788
Zarak <zarak@hotmail.ca>
2018-12-03 11:01:22 +0500

    First two sheets as csv

--- R/GM_share_price.R
+++ R/GM_share_price.R
@@ -1,7 +1,7 @@
 library(ggplot2)
 library(zoo)
 
-GM1 <- read.csv("~/Downloads/R-Project data set (raw 12-03-18) - GM 4Q10-3Q18.csv")
+GM1 <- read.csv("../data/R-Project data set (raw 12-03-18) - GM 4Q10-3Q18.csv")
 GM1 <- na.omit(GM1)
 colnames(GM1) <- c("Date", "Price")
 
