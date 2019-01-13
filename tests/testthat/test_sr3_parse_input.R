43cb6c5f6b039591aa64dc7b8a10530b581887ea
Zarak <zarak@hotmail.ca>
2019-01-13 21:25:27 +0500

    Refactor unit test

--- tests/testthat/test_sr3_parse_input.R
+++ tests/testthat/test_sr3_parse_input.R
@@ -1,10 +1,13 @@
 context('input parsing')
 
 test_that("parser returns a list of 18 elements", {
+  # Input validation for default arugments should
+  # work if this test passes.
   A <- matrix(0, 3, 4)
   b <- matrix(4, 1)
   m <- dim(A)[1]
   n <- dim(A)[2]
 
-  expect_equal(length(sr3_parse_input(A, b, m, n)), 18)
+  p_length <- length(sr3_parse_input(A, b, m, n))
+  expect_equal(p_length, 18)
 })
